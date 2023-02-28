702ce9a9242fc376e04bc58334071ec0077011f8
Taehwan Kim (Ken) <47441965+obtusa07@users.noreply.github.com>
2023-02-28 18:04:02 +0900

    [Silver III] Title: 소수 구하기, Time: 572 ms, Memory: 41628 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+import math
+
+m, n = map(int, input().split())
+array = [True for i in range(n + 1)]
+array[0] = False
+array[1] = False
+for i in range(2, int(math.sqrt(n)) + 1):
+    if array[i] == True:
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+
+for i in range(m, n+1):
+    if array[i]:
+        print(i)
